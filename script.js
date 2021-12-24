d424b6a46a05c7efc9e7c80a3ccbcf195bf1e66b
Alex Miramontes <alex.r.miramontes@gmail.com>
2021-12-23 16:28:28 -0800

    updated variables in js file

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCaseLetters = ["A", "B", "C"];
-var numbers = ["1", "2", "3"];
-var specialCharacters = ["!", "@", "#"];
+var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+"];
 
 
 
@@ -48,15 +48,19 @@ if (shouldContainCharacters) {
   pot = pot.concat(specialCharacters);
 }
 
-console.log(pot);
+
+
+
 
 // loop through randomly length times and create copies from the pot and add to our final password
-
-
-
-
-
-
+  //for loop run as many times as user input for password length
+  //generate a random index insdie the for loop
+  //use random index to select a character from pot array
+  //push copies of random characters to new array
+  //
+  //convert characters from new array to string without commas - use an array join - arraynamewhereyouhaveputthecopiedrandomcharacters.join("")
+  //return passwordstring from above
+  //
 
 
 
